8cdd93fd7de82f721c6ebddb8852a48643ae0414
hatomachikadai <e195728@ie.u-ryukyu.ac.jp>
2021-02-03 02:56:29 +0900

    ステップ2

--- app/src/main/java/jp/ac/uryukyu/ie/e195728/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e195728/Hero.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e195728;
 
+
 /**
  * ヒーロークラス。
  *  String name; //敵の名前
@@ -9,10 +10,10 @@ package jp.ac.uryukyu.ie.e195728;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -27,7 +28,21 @@ public class Hero {
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
-
+    public String getName(){
+        return name;
+    } 
+    public int getHitPoint(){
+        return hitPoint;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public boolean isDead(){
+        return dead;
+    }
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
     /**
      * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
@@ -35,7 +50,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
